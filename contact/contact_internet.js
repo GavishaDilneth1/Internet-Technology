3aa670170521462d6b2a2d14b7154aba453623ee
GavishaDilneth1 <liozen694@gail.com>
2025-10-30 16:39:01 +0530

    firstcommit

---
+++ contact/contact_internet.js
@@ -0,0 +1,33 @@
+ AOS.init({
+    once: false, 
+    duration: 1000, 
+  });
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
+const menuIcon = document.querySelector('.menu');
+const navBar = document.querySelector('.nav_bar');
+
+menuIcon.addEventListener('click', () => {
+    navBar.classList.toggle('active'); 
+});
+function openPopup() {
+  document.getElementById("popup").style.display = "flex";
+}
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
